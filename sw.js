24091e02b4e60cc2c9658a8e5fdd76fb28e5a123
PhoebeHpHsu <phoebehsu8@gmail.com>
2026-01-17 17:44:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-// 我們將版本號改為 v2，這會強迫瀏覽器重新下載檔案
-const CACHE_NAME = 'reading-log-v2'; 
+// 版本號更新為 v3，強制瀏覽器忘記舊的錯誤
+const CACHE_NAME = 'reading-log-v3'; 
 
 self.addEventListener('install', (e) => {
-  // 強制這個新的 Service Worker 立即進入「等待中」狀態，不需等待舊的關閉
-  self.skipWaiting(); 
+  self.skipWaiting(); // 強制啟用新版
   
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
@@ -15,11 +14,9 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  // 當這個新的 SW 啟動時，立即接管頁面控制權
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        // 刪除所有不是 v2 的舊快取
         if (key !== CACHE_NAME) {
           return caches.delete(key);
         }
